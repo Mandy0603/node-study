c413b090d47011d233497ebd65a153b39172451a
Mandy Ma <yandim0317@gmail.com>
2019-07-22 09:13:35 +1000

    Add config files

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,12 @@
 const request = require("request");
+const keys = require("./config/keys");
 
-const url =
-  "https://api.darksky.net/forecast/f5668a8c93b42c0dbc654e5883e9e931/37.8267,-122.4233";
-const url2 =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWFuZHltYSIsImEiOiJjanljaDNuNWowaW0yM25rNHBwczBrOWZlIn0.CZiMKMQFz3GwBRajPDXUGQ";
+const url = `https://api.darksky.net/forecast/${
+  keys.darkSkyKeys
+}/37.8267,-122.4233`;
+const url2 = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${
+  keys.mapBoxKeys
+}`;
 
 request({ url: url, json: true }, (error, response) => {
   console.log(response.body.currently);
