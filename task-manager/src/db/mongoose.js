193b9db634280fa6f3890c3b9e536b40107f76ab
Mandy Ma <yandim0317@gmail.com>
2019-07-26 17:17:23 +1000

    Added authentication.

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+mongoose.connect("mongodb://127.0.0.1/task-manager-api", {
   useNewUrlParser: true,
-  useCreateIndex: true
+  useCreateIndex: true,
+  useFindAndModify: false
 });
