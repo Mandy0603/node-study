a94dcaaaa9be8a329fddb0f5bc78eb48efc9e92a
Mandy Ma <yandim0317@gmail.com>
2019-07-29 10:38:15 +1000

    Finished task tests.

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require("../src/app");
 const User = require("../src/models/user");
-const { userOneId, userOne, setupDatabase } = require("./fixtures/db.js");
+const { userOneId, userOne, setupDatabase } = require("./fixtures/db");
 
 beforeEach(setupDatabase);
 
